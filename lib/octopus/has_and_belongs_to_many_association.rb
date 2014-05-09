ebf2aa8927e7f77773fbd51401022a3113646a92
Aubrey Holland <aubreyholland@gmail.com>
2014-05-09 17:18:23 -0400

    fixes for rails 4.1

--- lib/octopus/has_and_belongs_to_many_association.rb
+++
@@ -1,7 +0,0 @@
-module Octopus::HasAndBelongsToManyAssociation
-  def self.included(base)
-    base.sharded_methods :insert_record
-  end
-end
-
-ActiveRecord::Associations::HasAndBelongsToManyAssociation.send(:include, Octopus::HasAndBelongsToManyAssociation)
