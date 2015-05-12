50433a5931762469fe383f4dbfce5b5b39981314
Aubrey Holland <aubreyholland@gmail.com>
2015-05-12 15:10:55 -0400

    merge down from latest

--- lib/octopus/scope_proxy.rb
+++ lib/octopus/scope_proxy.rb
@@ -1,47 +1,61 @@
-class Octopus::ScopeProxy
-  include Octopus::ShardTracking::Attribute
-  attr_accessor :klass
+module Octopus
+  class ScopeProxy < BasicObject
+    include ::Octopus::ShardTracking::Attribute
 
-  def initialize(shard, klass)
-    @current_shard = shard
-    @klass = klass
-  end
-
-  def using(shard)
-    raise "Nonexistent Shard Name: #{shard}" if @klass.connection.instance_variable_get(:@shards)[shard].nil?
-    @current_shard = shard
-    return self
-  end
-
-  # Transaction Method send all queries to a specified shard.
-  def transaction(options = {}, &block)
-    run_on_shard { @klass = klass.transaction(options, &block) }
-  end
-
-  def connection
-    @klass.connection().current_shard = @current_shard
-    @klass.connection()
-  end
-
-  def method_missing(method, *args, &block)
-    result = run_on_shard { @klass.send(method, *args, &block) }
-
-    if result.respond_to?(:scoped)
-      @klass = result
-      return self
+    module CaseFixer
+      def ===(other)
+        other = other.klass while ::Octopus::ScopeProxy === other
+        super
+      end
     end
 
-    result
-  end
+    attr_accessor :klass
 
-  def as_json(options = nil)
-    method_missing(:as_json, options)
-  end
+    def initialize(shard, klass)
+      @current_shard = shard
+      @klass = klass
+    end
 
-  # Delegates to method_missing (instead of @klass) so that User.using(:blah).where(:name => "Mike")
-  # gets run in the correct shard context when #== is evaluated.
-  def ==(*args)
-    method_missing(:==, *args)
+    def using(shard)
+      fail "Nonexistent Shard Name: #{shard}" if @klass.connection.instance_variable_get(:@shards)[shard].nil?
+      @current_shard = shard
+      self
+    end
+
+    # Transaction Method send all queries to a specified shard.
+    def transaction(options = {}, &block)
+      run_on_shard { @klass = klass.transaction(options, &block) }
+    end
+
+    def connection
+      @klass.connection_proxy.current_shard = @current_shard
+
+      if @klass.custom_octopus_connection && @klass.allowed_shard?(@current_shard)
+        # Force use of proxy, given we called 'using' explicitly to get here
+        @klass.connection_proxy.current_model = @klass
+        @klass.connection_proxy
+      else
+        @klass.connection
+      end
+    end
+
+    def method_missing(method, *args, &block)
+      result = run_on_shard { @klass.send(method, *args, &block) }
+      if result.respond_to?(:all)
+        @klass = result
+        return self
+      end
+
+      result
+    end
+
+    # Delegates to method_missing (instead of @klass) so that User.using(:blah).where(:name => "Mike")
+    # gets run in the correct shard context when #== is evaluated.
+    def ==(other)
+      method_missing(:==, other)
+    end
+    alias_method :eql?, :==
   end
-  alias :eql? :==
 end
+
+ActiveRecord::Relation.extend(Octopus::ScopeProxy::CaseFixer)
